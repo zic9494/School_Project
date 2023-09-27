5710a98f2de1aa2aab81cd70c581bfe067205f92
zic9494 <98081424+zic9494@users.noreply.github.com>
2023-09-27 15:22:45 +0800

    HTML,JS,CSS

---
+++ JS/Chart.js
@@ -0,0 +1,235 @@
+var Show_Chart = {
+  
+  type: "pie",
+  data:{
+    labels: [],
+    datasets: {}
+  },
+  options: {
+    plugins: {
+      legend: {
+        labels: {
+          color: "rgb(255, 255, 255)",
+        }
+      },
+    },
+    scales: {
+      y: {
+        ticks: {
+          color: "rgb(255, 255, 255)",
+        },
+        border: {
+          color: "rgb(255, 255, 255)",
+        },
+        display: false,
+      },
+      x: {
+        ticks: {
+          color: "rgb(255, 255, 255)",
+        },
+        border: {
+          color: "rgb(255, 255, 255)",
+        },
+        display: false,
+      }
+    }
+  },
+
+}
+var Payment_Lables=["日常開銷", "固定支出", "娛樂開銷", "臨時開銷"]
+var ChartObject, ChartObject2
+var ctx = document.getElementById("ShowChart").getContext("2d")
+var ctx2 = document.getElementById("ShowChart2").getContext("2d")
+function Check_Data_Is_Loaded(){
+  Assets_Table = document.getElementById("Assets_table").innerHTML
+  if (Assets == null){
+      setTimeout(()=>Check_Data_Is_Loaded(), 200)
+    }else{
+      Month_Payment()
+    }
+}
+
+function NetAsset_Chart(){
+  document.getElementById("in_debt").style.display = "block"
+  document.getElementById("BarandLine").style.display = "none"
+  document.getElementById("Pie").style.display = "none"
+  var tmp = [], Dataset = [], labels=[], all_debt = 0, all_net = 0
+  var Cookie = Get_Cookie(), Data = NetAssest.Data
+  Table_change("bill_table", "assets_table")
+  if (ChartObject != undefined){
+    ChartObject.destroy()
+  }
+  if (ChartObject2 != undefined){
+    ChartObject2.destroy()
+  }
+  if (Cookie["in_debt"] == "true"){
+    var debts = Debt.Data
+    for(var i = 0 ; i < debts.length;i++){
+      if (!debts[i]["Settle"]){
+        all_debt += debts[i]["Principal"]
+      }
+    }
+  }
+  for (var i = 0;i < Data.length ;i++){
+    tmp.push(Data[i]["Month_NetAssets"]-all_debt+all_net)
+    labels.push(Data[i]["Month"].slice(0, 7))
+    all_net += Data[i]["Month_NetAssets"]
+  }
+  Dataset.push({
+    label : "淨資產累計圖",
+    data : tmp,
+    backgroundColor: [
+      'rgb(255, 255, 255)',
+    ],
+    borderColor: "rgb(255, 255, 255)"
+  })
+  Show_Chart["data"]["datasets"] = Dataset
+  Show_Chart["data"]["labels"] = labels
+  Show_Chart["type"] = "line"
+  Show_Chart["options"]["scales"]["y"]["display"] = true
+  Show_Chart["options"]["scales"]["x"]["display"] = true
+  ChartObject = new Chart(ctx, Show_Chart)
+}
+
+function Month_Payment(){
+  var tmp =[], Dataset=[]
+  Table_change("assets_table", "bill_table")
+  document.getElementById("in_debt").style.display = "none"
+  document.getElementById("Income_table").style.display = "none"
+  document.getElementById("Income_table").style.display = "table"
+  document.getElementById("BarandLine").style.display = "block"
+  document.getElementById("Pie").style.display = "block"
+  for (var i = 0;i < Payment_Lables.length;i++){tmp.push(Payment_Sum(Payment_Lables[i]))}
+  Dataset_object={
+    label : "本月支出",
+    data : tmp,
+    backgroundColor: [
+      'rgb(153, 0, 255)',
+      'rgb(255, 0, 204)',
+      'rgb(0, 178, 255)',
+      'rgb(0, 136, 255)'
+    ],
+    borderColor: "rgb(255, 255, 255)"
+  }
+  Dataset.push(Dataset_object)
+  Show_Chart["data"]["datasets"] = Dataset
+  Show_Chart["data"]["labels"] = Payment_Lables
+  change_Chart_type("bar", "line")
+}
+
+function Assets_Analyze(){
+  Table_change("bill_table", "assets_table")
+  document.getElementById("BarandLine").style.display = "none"
+  document.getElementById("Pie").style.display = "none"
+  document.getElementById("in_debt").style.display = "none"
+  var Data = Assets.Data, tmp=[NetAsses_Sum(), 0, 0, 0, 0, 0], AssetType=["活存", "定存", "債券", "股票", "房地產", "虛擬貨幣"]
+  var Dataset = []
+  if (ChartObject != undefined){
+    ChartObject.destroy()
+  }
+  if (ChartObject2 != undefined){
+    ChartObject2.destroy()
+  }
+  for (var i=0; i<Data.length; i++){
+    var x = 0
+    for (;x < 6;x++){
+      if (Data[i]["AssetType"]==AssetType[x] ){
+        break
+      }
+    }
+    tmp[x] += parseInt(Data[i]["Totalvalue"])
+  }
+  Dataset.push({
+    label : "資產分配圖",
+    data : tmp,
+    backgroundColor: [
+      'rgb(153, 0, 255)',
+      'rgb(255, 0, 204)',
+      'rgb(0, 178, 255)',
+      'rgb(136, 136, 0)',
+      'rgb(0, 189, 0)',
+      'rgb(0, 255, 255)'
+
+    ],
+    borderColor: "rgb(255, 255, 255)"
+  })
+  Show_Chart["data"]["datasets"] = Dataset
+  Show_Chart["data"]["labels"] = AssetType
+  Show_Chart["type"] = "pie"
+  Show_Chart["options"]["scales"]["y"]["display"] = false
+  Show_Chart["options"]["scales"]["x"]["display"] = false
+  ChartObject = new Chart(ctx, Show_Chart)
+}
+
+function Payment_Sum(kind){
+  var Sum = 0
+  const Data =Payment.Data
+  for (var i = 0;i < Data.length;i++){
+    if (Data[i].Price < 0 && Data[i].PayWay == kind){
+      Sum -= Data[i].Price
+    }
+  }
+  return Sum
+}
+
+function NetAsses_Sum(){
+  var NetAssets_Data = NetAssest.Data, Sum = 0
+  for (var i = 0;i < NetAssets_Data.length; i++){
+    Sum += NetAssets_Data[i].Month_NetAssets
+  }
+  return Sum
+}
+
+function change_Chart_type(ChartType, ChartType2){
+  document.getElementById("ShowChart2").style.display="none"
+  if (ChartObject != undefined){
+    ChartObject.destroy()
+  }
+  if (ChartObject2 != undefined){
+    ChartObject2.destroy()
+  }
+  
+  if (Show_Chart["data"]["datasets"][0]["label"] == "本月支出"){
+    Show_Chart.data.labels = Payment_Lables
+  }
+  
+  if (ChartType != "pie"){
+    Show_Chart["options"]["scales"]["y"]["display"] = true
+    Show_Chart["options"]["scales"]["x"]["display"] = true
+  }else{
+    Show_Chart["options"]["scales"]["y"]["display"] = false
+    Show_Chart["options"]["scales"]["x"]["display"] = false
+  }
+  Show_Chart.type = ChartType
+  ChartObject = new Chart(ctx, Show_Chart)
+  
+  if (ChartType2 != undefined){
+    if (ChartType2 != "pie"){
+      Show_Chart["options"]["scales"]["y"]["display"] = true
+      Show_Chart["options"]["scales"]["x"]["display"] = true
+    }else{
+      Show_Chart["options"]["scales"]["y"]["display"] = false
+      Show_Chart["options"]["scales"]["x"]["display"] = false
+    }
+    document.getElementById("ShowChart2").style.display="block"
+    Show_Chart.type = ChartType2
+    ChartObject2 = new Chart(ctx2, Show_Chart)
+  }
+}
+
+function Table_change(none, block){
+  var none_table = document.getElementsByClassName(none)
+  var block_table =document.getElementsByClassName(block)
+  for (var i = 0; i < none_table.length; i++){
+    none_table[i].style.display = "none"
+  }
+  for (var i = 0;i < block_table.length; i++){
+    block_table[i].style.display = "table"
+  }
+}
+document.getElementById("in_debt").addEventListener("click", function(e){
+  var box = document.getElementById("debt_box")
+  Data= "in_debt="+box.checked+";"
+  document.cookie = Data
+  NetAsset_Chart()
+})
---
+++ JS/HomePage.js
@@ -0,0 +1,327 @@
+var Payment, Debt, Assets, NetAssest, SumNetAssest = 0
+var Search_Year, Search_Month
+
+window.onload=function(){
+    var time = new Date(Date.now())
+    Search_Year = time.getFullYear()
+    Search_Month = time.getMonth()+1
+    Date_Deal()
+    Get_Payment_data(Search_Year, Search_Month)
+    Get_Debt_Data()
+    Get_Netassets_Data()
+    Get_Assets_Data()
+    Check_Data_Is_Loaded()
+}
+function Date_Deal(){
+    document.getElementsByName("target_month")[0].value = Search_Month
+    var TimeTag = document.getElementById("Search")
+    TimeTag.innerHTML = Search_Year+" / "+Search_Month+TimeTag.innerHTML
+}
+function loadScript(url){    
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    head.appendChild(script);
+}
+function Get_Cookie(){
+    var data={}
+    var Str = document.cookie
+    Str = Str.split("; ")
+    for (var i = 0;i < Str.length; i++){
+        tmp = Str[i].split("=")
+        data[tmp[0]]=tmp[1]
+    }
+    return data
+}
+var drawer = document.getElementById("drawer")
+drawer.addEventListener("click",function(){
+    var user_info = document.getElementById("user_info")
+    var user_info_display = window.getComputedStyle(user_info,"dispaly").getPropertyValue("display")
+    if (user_info_display=="none"){
+        user_info.style.display = "block"
+    }else{
+        user_info.style.display = "none"
+    }
+})
+
+var Delete_Check= document.getElementsByName("delete")[0]
+Delete_Check.addEventListener("click",event=>{
+    if(event.target.checked){
+        alert("You are deleting data, please make sure your action is true")
+        document.getElementsByName("delete")[0].value = "true"
+    }else{
+        document.getElementsByName("delete")[0].value = "false"
+    }
+})
+
+var Settle_Check= document.getElementsByName("settle")[0]
+Settle_Check.addEventListener("click",event=>{
+    if(event.target.checked){
+        Settle_Check.value="true"
+    }else{
+        Settle_Check.value = "false"
+    }
+})
+
+function month_move(move){
+    Get_Payment_data()
+}
+
+function LogOut(){
+    window.location.replace("/user/logout")
+}
+
+function FormChage(what, values, Index, ServerID){
+    tmp = document.getElementsByClassName("debt")
+    DispalyChange(tmp,"none")
+    tmp = document.getElementsByClassName("payment")
+    DispalyChange(tmp,"none")
+    tmp = document.getElementsByClassName("assets")
+    DispalyChange(tmp,"none")
+    tmp = document.getElementsByClassName("income")
+    DispalyChange(tmp,"none")
+    document.getElementById("delete_checkbox").style.display = "none"
+    if (what != undefined){
+        document.getElementById("close_form").style.display = "block"
+        document.getElementById("submit").style.display="block"
+        document.getElementById("content").style.display="block"
+    }else{
+        document.getElementById("close_form").style.display = "none"
+        document.getElementById("submit").style.display="none"
+        document.getElementById("content").style.display="none"
+    }
+    tmp = document.getElementsByClassName(what)
+    DispalyChange(tmp,"block")
+    document.getElementById("ReturnType").value = what
+    if (values != undefined && ServerID != undefined && Index!=undefined ){
+        for (var i=0;i<Index.length;i++){
+            document.getElementsByName(Index[i])[0].value=values[i]
+        }
+        document.getElementById("Form").action = "/bill/update"
+        document.getElementsByName("TargetId")[0].value = ServerID
+        document.getElementById("delete_checkbox").style.display = "block"
+    }
+}
+
+function DispalyChange(elem, Into){
+    for (i = 0;i<elem.length;i++){
+        elem[i].style.display = Into
+    }
+}
+
+
+function Get_Payment_data(year, month){
+    var url = "/data/payment?year="+year+"&month="+month
+    console.log(url);
+    var link_payment = new XMLHttpRequest
+    link_payment.open("GET",url)
+    link_payment.send()
+    link_payment.onload = function(){
+        Payment = JSON.parse(link_payment.responseText)
+    }
+    document.getElementById("Payment_table").innerHTML = "Loading"
+    setTimeout(()=>Stop_Loading(Payment,  "Payment"),500)
+    document.getElementById("Income_table").innerHTML = "Loading"
+    setTimeout(()=>Stop_Loading(Payment, "Payment"),500)
+}
+
+function Get_Debt_Data(){
+    var link_debt = new XMLHttpRequest
+    link_debt.open("GET", "/data/debt")
+    link_debt.send()
+    link_debt.onload =function(){
+        Debt = JSON.parse(link_debt.responseText) 
+    }
+    document.getElementById("Debt_table").innerHTML = "Loading"
+    setTimeout(()=>Stop_Loading(Debt, "Debt"),500)
+}
+
+function Get_Assets_Data(){
+    var link_assets=new XMLHttpRequest
+    link_assets.open("GET","/data/assets")
+    link_assets.send()
+    link_assets.onload = function (){
+        Assets = JSON.parse(link_assets.responseText)
+    }
+    document.getElementById("Assets_table").innerHTML="Loading"
+    setTimeout(()=>{Stop_Loading(Assets, "Assets")}, 500)
+}
+
+function Get_Netassets_Data(){
+    var link_netassets=new XMLHttpRequest
+    link_netassets.open("GET","/data/netassets")
+    link_netassets.send()
+    link_netassets.onload = function (){
+        NetAssest = JSON.parse(link_netassets.responseText)
+    }
+    document.getElementById("Netassets_table").innerHTML="Loading"
+    setTimeout(()=>{Stop_Loading(NetAssest, "Netassets")}, 500)
+}
+
+
+function Stop_Loading(Item, who){
+    if (Item == null ){
+        switch(who){
+            case "Payment":{setTimeout(()=>Stop_Loading(Payment,"Payment"),500); break}
+            case "Debt":{setTimeout(()=>Stop_Loading(Debt, "Debt"),500); break}
+            case "Assets":{setTimeout(()=>Stop_Loading(Assets, "Assets"),500); break}
+            case "Netassets":{setTimeout(()=>Stop_Loading(Assets, "Assets"),500); break}
+        }
+    }else{
+        switch(who){
+            case "Payment":{setTimeout(Show_Payment_Data(), 500);break}
+            case "Debt":{setTimeout(()=>Show_Debt_Data(), 500);break}
+            case "Assets":{setTimeout(()=>Show_Assests_Data(), 500);break}
+            case "Netassets":{setTimeout(()=>Show_Netassets_Data(), 500);break}
+        }
+    }
+}
+
+function Show_Payment_Data(){
+    if (Payment== null){
+        setTimeout(()=>Show_Payment_Data(), 600)
+    }else{
+        var Payment_Table = document.getElementById("Payment_table")
+        var Income_Table = document.getElementById("Income_table")
+        var Payment_Data = Payment.Data
+        var Tmp_Str = ""
+        var Payment_Str="<h2>Payment</h2><tr><th>Price</th><th>Date</th><th>pay on</th><th>Content</th><th>edit</th>"
+        var Income_Str="<h2>Income</h2><tr><th>Price</th><th>Date</th><th>Content</th><th>edit</th>"
+        for (var i=0; i<Payment_Data.length;i++){
+            if (Payment_Data[i].Price<0){
+                Tmp_Str += "<tr><th>"
+                Tmp_Str += Payment_Data[i].Price*-1+"</th><th>"
+                Tmp_Str += Payment_Data[i].Date+"</th><th>"
+                Tmp_Str += Payment_Data[i].PayWay+"</th><th>"
+                Tmp_Str += Payment_Data[i].Content+"</th>"
+                Tmp_Str += "<th onclick='Edit_Btn(\"payment\","+i+")'>:</th></tr>"
+                Payment_Str += Tmp_Str
+                Tmp_Str = ""
+            }else{
+                continue
+            }
+        }
+        for (var i=0; i<Payment_Data.length;i++){
+            if (Payment_Data[i].Price>0){
+                Tmp_Str += "<tr><th>"
+                Tmp_Str += Payment_Data[i].Price+"</th><th>"
+                Tmp_Str += Payment_Data[i].Date+"</th><th>"
+                Tmp_Str += Payment_Data[i].Content+"</th>"
+                Tmp_Str += "<th onclick='Edit_Btn(\"income\","+i+")'>:</th></tr>"
+                Income_Str += Tmp_Str
+                Tmp_Str = ""
+            }else{
+                continue
+            }
+        }
+        Payment_Table.innerHTML = Payment_Str
+        Income_Table.innerHTML = Income_Str
+    }
+}
+
+function Show_Debt_Data(){
+    if(Debt==null){
+        setTimeout(()=>Show_Debt_Data(), 600)
+    }else{
+        var Debt_Table = document.getElementById("Debt_table")
+        var Debt_Data = Debt.Data
+        var Tmp_Str = ""
+        var Debt_Str = "<h2>Debt</h2><tr><th>Principal</th><th>Interest</th><th>Attribute</th><th>Settle</th><th>Content</th><th>edit</th></tr>"
+        for(var i=0;i<Debt_Data.length;i++){
+            Tmp_Str += "<tr><th>"
+            Tmp_Str += Debt_Data[i].Principal+"</th><th>"
+            Tmp_Str += Debt_Data[i].Interest+"</th><th>"
+            Tmp_Str += Debt_Data[i].Attribute+"</th><th>"
+            if (Debt_Data[i].Settle){
+                Tmp_Str += "已清償</th><th>"
+            }else{
+                Tmp_Str += "償還中</th><th>"
+            }
+            Tmp_Str += Debt_Data[i].Content+"</th>"
+            Tmp_Str += "<th onclick='Edit_Btn(\"debt\","+i+")'>:</th>"
+            Debt_Str+=Tmp_Str
+            Tmp_Str=""
+        }
+        Debt_Table.innerHTML=Debt_Str
+    }
+}
+
+function Show_Assests_Data(){
+    if (Assets== null){
+        setTimeout(()=>Show_Assests_Data(), 600)
+    }else{
+        var Assets_Table = document.getElementById("Assets_table")
+        var Assets_Data = Assets.Data
+        var Assets_Str = "<h2>Assets</h2><tr><th>Total Volue</th><th>Asset Type</th><th>Currency Type</th><th>Content</th><th>edit</th></tr>"
+        var Tmp_Str = "<tr><th>"+SumNetAssest+"</th><th>活存</th><th>TWD</th><th>每月結餘</th><th>系統自動生成</th>"
+        for (var i=0;i<Assets_Data.length;i++){
+            Tmp_Str += "<tr><th>"
+            Tmp_Str += Assets_Data[i].Totalvalue+"</th><th>"
+            Tmp_Str += Assets_Data[i].AssetType+"</th><th>"
+            Tmp_Str += Assets_Data[i].CurrencyType+"</th>"
+            Tmp_Str += "<th>"+Assets_Data[i].Content+"</th>"
+            Tmp_Str += "<th onclick='Edit_Btn(\"assets\","+i+")'>:</th>"
+            Assets_Str += Tmp_Str
+            Tmp_Str = ""
+        }
+        Assets_Table.innerHTML = Assets_Str;
+    }
+}
+
+function Show_Netassets_Data(){
+    if (NetAssest == null){
+        setTimeout(()=>Show_Netassets_Data(), 500)
+    }else{
+        var NetAssets_Table = document.getElementById("Netassets_table")
+        var NetAssets_Data = NetAssest.Data
+        var NetAssets_Str = "<h2>Net Assets</h2><th>Balance</th><th>Month</th></tr>"
+        var Tmp_Str = ""
+        for (var i = 0;i < NetAssets_Data.length; i++){
+            var Month = NetAssets_Data[i].Month 
+            Tmp_Str += "<tr><th>"
+            Tmp_Str += NetAssets_Data[i].Month_NetAssets+"</th><th>"
+            Tmp_Str += Month.substr(0, 7)+"</th>"
+            NetAssets_Str += Tmp_Str
+            Tmp_Str = ""
+            SumNetAssest += NetAssets_Data[i].Month_NetAssets
+        }
+        NetAssets_Table.innerHTML = NetAssets_Str
+    }
+}
+
+function Edit_Btn(what, Table_Id){
+    var value_array, Data, Index_array
+    switch(what){
+        case "payment": {
+            Data = Payment.Data[Table_Id]
+            value_array = [Data.Price*-1, Data.Date, Data.PayWay, Data.Content]
+            Index_array = ["price", "date", "payway", "content"]
+            break;
+        }
+        case "income":{
+            Data = Payment.Data[Table_Id]
+            value_array = [Data.Price, Data.Date, Data.Content]
+            Index_array = ["price", "date", "content"]
+            break;
+        }
+        case "debt":{
+            Data = Debt.Data[Table_Id]
+            value_array = [Data.Principal, Data.Interest, Data.Attribute, Data.Settle, Data.Content]
+            Index_array = ["principal", "interest", "attribute", "settle", "content"]
+            document.getElementsByName("settle")[0].checked = Data.Settle
+            break;
+        }
+        case "assets":{
+            Data = Assets.Data[Table_Id]
+            value_array = [Data.Totalvalue, Data.Content, Data.CurrencyType, Data.AssetType]
+            Index_array = ["total_volue", "content", "currencytype", "assetstype"]
+            break;
+        }
+        default:{
+            console.log("Nothing happend");
+            return
+        }
+    }
+    FormChage(what, value_array, Index_array, Data.Id)
+}
